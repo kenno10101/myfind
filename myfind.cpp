3f88e33e404c0c9686389b8545bcf62912598730
Kenn-Michael Sanga <kennsanga@gmail.com>
2024-11-09 19:31:06 +0100

    started search logic only (not recursive, only in current directory), added test directories + files

--- myfind.cpp
+++ myfind.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <string>
 #include <unistd.h> // für getopt()
+#include <dirent.h> // für directory operations
 
 namespace fs = std::filesystem;
 
@@ -38,17 +39,52 @@ int main(int argc, char *argv[]) {
         print_usage(program_name);
     }
 
+    DIR *dirp;
+    dirp = opendir(argv[optind]);
+
     fs::path searchpath = argv[optind++];
     if (!fs::exists(searchpath) || !fs::is_directory(searchpath)) {
         std::cerr << "Error: " << searchpath << " is not a valid directory.\n";
         return EXIT_FAILURE;
     }
 
+    std::string filename = argv[optind++];
+    if (filename.length() == 0) {
+        std::cerr << "Error: " << filename << " is missing.\n";
+        print_usage(program_name);
+        return EXIT_FAILURE;
+    }
+
+
     // TODO: Implement the search logic here
+
+
+    bool directory_open_fail = dirp == NULL;
+    if (directory_open_fail)
+    {
+        perror("failed to open directory");
+        return 1;
+    }
+
+    std::cout << "opened directory" << std::endl << "reading contents of opened directory:" << std::endl;
+    
+    struct dirent *direntp;
+    bool file_found = false;
+    while ((direntp = readdir(dirp)) != NULL){
+        std::cout << direntp->d_name << std::endl;
+        if (filename == direntp->d_name){
+            file_found = true;
+        }
+    }
+
+    if (file_found){
+        std::cout << "file was found in given directory" << std::endl;
+    } else {
+        std::cout << "file does not exist in given directory" << std::endl;
+    }
+      
     
     // TODO: Child process for each filepath 
-
-    // TODO : Change Makefile
 
 
     return EXIT_SUCCESS;
